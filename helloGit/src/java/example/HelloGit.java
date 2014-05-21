e361018a8ca09d7773ecfadda683afc5fcb720aa
Sergio <Sergio@192.168.52.118>
2014-05-21 10:28:48 -0500

    primer commit

---
+++ helloGit/src/java/example/HelloGit.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+/**
+ *
+ * @author Sergio
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+    }
+}
